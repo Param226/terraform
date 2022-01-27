58bfd85a8d7096bda23aa4d6f94041a73554eb55
Param226 <69100212+Param226@users.noreply.github.com>
2022-01-27 14:56:00 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,15 +1,1 @@
-provider "aws" {
-        access_key = "AKIA2QZ3M7OMCQQM423M"
-        secret_key = "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"
-        region = "ap-south-1"
-}
-resource "aws_instance" "myfirst_instance" {
-        ami = "ami-0af25d0df86db00c1"
-        count = 1
-        key_name = "key"
-        instance_type = "t2.micro"
-        security_groups = ["my_security_group"]
-        tags = {
-                Name = "terraform_instance"
-        }
-}
+
